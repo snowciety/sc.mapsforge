e99e46c0b89f6c61367ca8d7e90c9554185f6693
Erik Lindqvist <eriksmejl@gmail.com>
2013-07-17 11:05:03 +0200

    Update example code with polygon

--- example/app.js
+++ example/app.js
@@ -18,18 +18,21 @@ win.open();
 //mapView.setCenter(47.32, 12.79); //Change current center
 //mapView.setZoomLevel("12"); //Change current zoom level
 mapView.addLayer({ 
-	"name": "snowciety", 
-	"url": "http://{s}.tiles.cdn.snowcietyapp.com/snowciety/{z}/{x}/{y}.png",
-	"subdomains": ["a", "b", "c"],
+	"name": "osm", 
+	"url": "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+	"subdomains": ["a", "b"],
 	"parallelrequests": 2,
 	"maxzoom": "18",
 	"minzoom": "12"
 	});
-mapView.startLayer("snowciety"); //All layers can be started with .startLayers()
-//Draws to triangles on the map
+mapView.startLayer("osm"); //All layers can be started with .startLayers()
+//Draw a blue triangle on the map
 mapView.drawPolyline({
 	"coordinates": [[47.33,12.76],[47.33,12.78], [47.35, 12.77],[47.33,12.76]], 
 	"color": "blue"});
-mapView.drawPolyline({
-	"coordinates": [[47.3345,12.7645],[47.3345,12.7845], [47.3545, 12.7745],[47.3345,12.7645]], 
-	"color": "red"});
+//Draw a green polygon with a thick black stroke
+mapView.drawPolygon({
+	"coordinates": [[47.3045, 12.7345], [47.3045, 12.7545], [47.3235, 12.7545], [47.3235, 12.7345], [47.3045, 12.7345]], 
+	"fillcolor": "green",
+	"strokecolor": "black",
+	"strokewidth": 5});
